9ba2b1cb41d773c483a05fa3188ea6e20e2a8b94
Raunak <raunaksomani321@gmail.com>
2021-04-18 21:32:30 +0530

    after updates

--- shop/views.py
+++ shop/views.py
@@ -7,14 +7,22 @@ def index(request):
     # products  = Product.objects.all()
     allProds=[]
     catprods = Product.objects.values('category','id')
-    cats = {item['category'] for item in catprods}
+    #  iise hm category and id le rahe h catprods m 
+    # cats = {item['category'] for item in catprods}
+    # print(catprods)
+    cats = set()
+    for item in catprods:
+        cats.add(item['category'])
+        
     for cat in cats:
-        prod = Product.objects.filter(category=cat)
+        prod = Product.objects.filter(category=cat) 
+        # iska matlab h sirf vohi object do jinki category cat h
         n = len(prod)
         n_slides = n//4 + ceil((n/4)-(n//4))
         allProds.append([prod,range(1,n_slides),n_slides])
     
     params = {"allProds" : allProds}
+    # as we can pass only dictionary thats why we are writing like this 
     
     return render(request,'shop/index.html',params)
 def about(request):
